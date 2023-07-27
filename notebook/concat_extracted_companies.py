1b4b014dfc5a6786885d26c181c400145640d537
YACI01 <130329130+YACI01@users.noreply.github.com>
2023-07-27 09:37:39 +0200

    Mise à jour du notebook

--- notebook/concat_extracted_companies.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-def concat_extracted_companies(df_company, df_cofidis, df_orange, df_floa, df_bourso, df_anytime):
-    df_all = pd.concat([df_company, df_cofidis, df_orange, df_floa, df_bourso, df_anytime], axis=0)
-    df_all = df_all.reset_index(drop=True)
-    return df_all
