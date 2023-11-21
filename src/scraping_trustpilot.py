9c03f8d3ee3ad70ec1e5812cfaa68cf85d37af5a
Zul <zulgit@gmail.com>
2023-11-21 13:15:05 +0100

    Correction du bug sur le scraping de la langue.

--- src/scraping_trustpilot.py
+++ src/scraping_trustpilot.py
@@ -29,7 +29,8 @@ def extract_reviews(url):
             review_data["Company"] = soup.find("h1", class_="typography_default__hIMlQ typography_appearance-default__AAY17 title_title__i9V__").span.get_text(strip=True)
             review_data["Customer"] = review.find("span", class_="typography_heading-xxs__QKBS8 typography_appearance-default__AAY17").text
             review_data["Number_review"] = review.find("div", class_="styles_consumerExtraDetails__fxS4S").find("span").text
-            review_data["Language"] = review.find("div", class_="typography_body-m__xgxZ_ typography_appearance-subtle__8_H2l styles_detailsIcon__Fo_ua").text
+            language_element = review.find("div", class_="typography_body-m__xgxZ_ typography_appearance-subtle__8_H2l styles_detailsIcon__Fo_ua")
+            review_data["Language"] = language_element.text if language_element else None
             review_data["Title"] = review.find("h2", class_="typography_heading-s__f7029 typography_appearance-default__AAY17").text
             review_data["Date_review"] = review.find("div", class_="styles_reviewHeader__iU9Px").time.text
             reply = review.find("div").find("p", class_="typography_body-m__xgxZ_ typography_appearance-default__AAY17 styles_message__shHhX")
